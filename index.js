45be3b8f7328d7543c46ef84280058cdf65e855b
gpitrella <gabriel98_@hotmail.com>
2024-09-28 11:13:34 -0300

    update practicum

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+// * Code for Route 1 goes here:
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -25,7 +27,7 @@ const PRIVATE_APP_ACCESS = '';
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
-* * App.get sample
+* * App.get sample */
 app.get('/contacts', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
@@ -41,7 +43,41 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
+// App.get sample 
+app.get('/pets', async (req, res) => {
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/pets';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        console.log('Data Pets:', data)
+        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// App.get sample 
+app.get('/companies', async (req, res) => {
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/companies';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        console.log('Data Companies:', data)
+        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+/* * App.post sample */
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
@@ -64,7 +100,7 @@ app.post('/update', async (req, res) => {
     }
 
 });
-*/
+
 
 
 // * Localhost
